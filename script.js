3fb281b59471c4c3e8bb3f473983441aee437997
Lucas <li,lucasinocencio@gmail.com>
2025-02-16 12:20:20 -0300

    fifth commit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
@@ -16,21 +17,28 @@ const asYouWere = {
   songName: 'As You Were',
   artist: 'TrackTribe',
   file: 'as_you_were',
+  liked: false,
 };
 const boomBapFlick = {
   songName: 'Boom Bap Flick',
   artist: 'Quincas Moreira',
   file: 'boom_bap_flick',
+  liked: true,
 };
 const cantHide = {
   songName: "Can't Hide",
   artist: 'Otis Mcdonald',
   file: 'cant_hide',
+  liked: false,
 };
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPlaylist = [asYouWere, boomBapFlick, cantHide];
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [
+  asYouWere,
+  boomBapFlick,
+  cantHide,
+];
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
@@ -56,11 +64,24 @@ function playPauseDecider() {
   }
 }
 
+function likeButtonRender() {
+  if (sortedPlaylist[index].liked === true) {
+    likeButton.querySelector('.bi').classList.remove('bi-heart');
+    likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+    likeButton.classList.add('button-active');
+  } else {
+    likeButton.querySelector('.bi').classList.add('bi-heart');
+    likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+    likeButton.classList.remove('button-active');
+  }
+}
+
 function initializeSong() {
   cover.src = `images/${sortedPlaylist[index].file}.webp`;
   song.src = `songs/${sortedPlaylist[index].file}.mp3`;
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
+  likeButtonRender();
 }
 
 function previousSong() {
@@ -143,13 +164,25 @@ function toHHMMSS(originalNumber) {
   let min = Math.floor((originalNumber - hours * 3600) / 60);
   let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
 
-  return `${hours.toString().padStart(2, '0')}:${min
+  console.log('hours: ', hours);
+
+  return `${hours !== 0 ? hours.toString().padStart(2, '0') + ':' : ''}${min
     .toString()
     .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
 
 function updateTotalTime() {
   totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function likeButtonClicked() {
+  if (sortedPlaylist[index].liked === false) {
+    sortedPlaylist[index].liked = true;
+  } else {
+    sortedPlaylist[index].liked = false;
+  }
+  likeButtonRender();
+  localStorage.setItem('playlist', JSON.stringify(originalPlaylist));
 }
 
 initializeSong();
@@ -163,3 +196,4 @@ song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked);
